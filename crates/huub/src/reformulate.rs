8c24ae5c9dcb332f1998258c0114858be114ab7d
Allen Zhong <zhongzhw6@gmail.com>
2025-06-07 00:16:29 +1000

    add initial implementation of cumulative with fixed durations and resource usages

--- crates/huub/src/reformulate.rs
+++ crates/huub/src/reformulate.rs
@@ -20,6 +20,7 @@ use crate::{
 	},
 	constraints::{
 		bool_array_element::BoolDecisionArrayElement,
+		cumulative::Cumulative,
 		disjunctive_strict::DisjunctiveStrict,
 		int_abs::IntAbs,
 		int_all_different::IntAllDifferent,
@@ -94,6 +95,7 @@ pub(crate) enum BoolDecisionInner {
 pub(crate) enum ConstraintStore {
 	BoolDecisionArrayElement(BoolDecisionArrayElement),
 	BoolFormula(BoolFormula),
+	Cumulative(Cumulative),
 	DisjunctiveStrict(DisjunctiveStrict),
 	IntAbs(IntAbs),
 	IntAllDifferent(IntAllDifferent),
@@ -263,6 +265,9 @@ impl ConstraintStore {
 			}
 			ConstraintStore::BoolFormula(exp) => {
 				<Formula<BoolDecision> as Constraint<Model>>::to_solver(exp, &mut actions)
+			}
+			ConstraintStore::Cumulative(con) => {
+				<Cumulative as Constraint<Model>>::to_solver(con, &mut actions)
 			}
 			ConstraintStore::DisjunctiveStrict(con) => {
 				<DisjunctiveStrict as Constraint<Model>>::to_solver(con, &mut actions)
